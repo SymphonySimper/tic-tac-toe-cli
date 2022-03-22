2b55a746da9435f54a1df7dcef558d6305f6edb0
SymphonySimper <SymphonySimper@users.noreply.github.com>
2022-03-22 08:47:38 +0530

    organizing and added clear screen

--- src/game.rs
+++ src/game.rs
@@ -1,100 +1,43 @@
-use std::io::{stdin, stdout, Write};
+use crate::Logic;
 
 pub struct Game {
-    pub board: Vec<char>,
-    pub turn: char,
+    logic: Logic,
+    round: i8,
 }
-
 impl Game {
     pub fn new() -> Self {
         Self {
-            board: vec![' '; 9],
-            turn: 'X',
+            logic: Logic::new(),
+            round: 1,
         }
     }
-    pub fn get_input(&self) -> Option<usize> {
-        let mut n = String::new();
-        println!("It's player {}'s turn: ", self.turn);
-        print!("Enter your choice [0-8]: ");
-        stdout().flush().expect("Couldn't flush to stdout");
-        stdin().read_line(&mut n).expect("Could't read from stdin");
-        let n = n.trim().parse::<usize>();
-        if let Ok(i) = n {
-            if i > 8 {
-                None
+
+    pub fn run(&mut self) {
+        let ref mut logic = self.logic;
+        loop {
+            if self.round > 9 {
+                println!("It's a tie");
+                break;
+            }
+
+            // Clear screen
+            println!("\x1Bc");
+
+            logic.display();
+            let m = logic.get_input();
+            if let Some(i) = m {
+                logic.update(i);
             } else {
-                Some(i)
+                println!("Enter a valid choice![0-8]");
+                continue;
             }
-        } else {
-            None
-        }
-    }
-
-    pub fn display(&self) {
-        let line = "-".repeat(13);
-        println!("{}", line);
-        let mut n = 0;
-        for c in &self.board {
-            if n == 0 {
-                print!("| ")
-            }
-            if n <= 2 && n > 0 {
-                print!(" | ")
-            }
-            print!("{}", c);
-            if n == 2 {
-                print!(" |");
-                println!("\n{}", line);
-                n = 0;
+            if logic.is_done() {
+                println!("Payer {} won!", logic.turn);
+                break;
             } else {
-                n += 1
+                logic.change_turn();
+                self.round += 1;
             }
         }
-        println!();
-    }
-
-    pub fn update(&mut self, n: usize) {
-        if self.board[n] == ' ' {
-            self.board[n] = self.turn;
-        }
-    }
-
-    pub fn change_turn(&mut self) {
-        if self.turn == 'X' {
-            self.turn = 'O'
-        } else {
-            self.turn = 'X'
-        }
-    }
-
-    pub fn is_done(&self) -> bool {
-        let win_combi = vec![
-            // horizontal
-            (0, 1, 2),
-            (3, 4, 5),
-            (6, 7, 8),
-            // vertical
-            (0, 3, 6),
-            (1, 4, 7),
-            (2, 5, 8),
-            // plus
-            (1, 4, 7),
-            (3, 4, 5),
-            // cross
-            (0, 4, 8),
-            (2, 4, 6),
-        ];
-
-        let mut win = false;
-        for &(x, y, z) in win_combi.iter() {
-            if self.board[x] == self.board[y]
-                && self.board[y] == self.board[z]
-                && self.board[z] == self.turn
-            {
-                win = true;
-            }
-        }
-
-        win
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,8 @@
+mod logic;
 mod game;
+pub use logic::Logic;
 use game::Game;
 
 fn main() {
-    let mut game = Game::new();
-    loop {
-        game.display();
-        let m = game.get_input();
-        if let Some(i) = m {
-            game.update(i);
-        } else {
-            println!("Enter a valid choice![0-8]");
-            continue;
-        }
-        if game.is_done() {
-            println!("Payer {} won!", game.turn);
-            break;
-        } else {
-            game.change_turn()
-        }
-    }
+    Game::new().run()
 }
